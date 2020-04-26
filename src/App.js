f0f5f33428008fa11f5bfe335511a884a0d19bb8
Shrish <b.shrish12@gmail.com>
2020-04-26 11:06:59 +1000

    update configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import JobBoard from "./Components/JobBoard";
+import JobBoard from "./components/JobBoard";
 import data from "./mockdata/data.json";
 
 function App() {
