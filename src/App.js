ab768c586b0668fbebbd111d8ec2ea281e705fb9
Shrish <b.shrish12@gmail.com>
2020-04-26 23:01:29 +1000

    add filtering

--- src/App.js
+++ src/App.js
@@ -4,15 +4,61 @@ import data from "./mockdata/data.json";
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
     setJobs(data);
   }, []);
 
+  const filterFunc = ({ role, level, tools, languages }) => {
+    const tags = [role, level];
+
+    if (filters.length === 0) {
+      return true;
+    }
+
+    if (tools) {
+      tags.push(...tools);
+    }
+
+    if (languages) {
+      tags.push(...languages);
+    }
+    return tags.some((tag) => filters.includes(tag));
+  };
+
+  const filteredJobs = jobs.filter(filterFunc);
+
+  const handleTagClick = (tag) => {
+    setFilters([...filters, tag]);
+  };
+
+  const handleFilterClick = (clickedFiler) =>{
+    setFilters(filters.filter((f) => f!== clickedFiler))
+  };
+
   return (
     <div className="App">
+      <header className="bg-teal-500 mb-12">
+        <img src="/images/bg-header-desktop.svg" alt="header" />
+      </header>
+      <div className="flex bg-white shadow-md rounded my-16 mx-10 p-6">
+        {filters.length > 0 &&
+          filters.map((filter) => (
+            <span
+            className="mx-2"
+            onClick={() => handleFilterClick(filter)}
+            >
+            <span className="p-2 bg-teal-100 text-teal-500 font-bold rounded"> {filter}</span>      
+            <span className="cursor-pointer p-2 bg-teal-500 bg-teal-300">X</span>
+            </span>
+          ))}
+      </div>
+
       {jobs.length > 0 ? (
-        jobs.map((job) => <JobBoard job={job} key={job.id} />)
+        filteredJobs.map((job) => (
+          <JobBoard job={job} key={job.id} handleTagClick={handleTagClick} />
+        ))
       ) : (
         <p> Jobs are loading...</p>
       )}
--- src/Components/JobBoard.js
+++ src/Components/JobBoard.js
@@ -1,10 +1,69 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const JobBoard = (props) => {
+const JobBoard = ({ job,handleTagClick }) => {
+  const {
+    logo,
+    company,
+    postedAt,
+    position,
+    contract,
+    location,
+    new: newJob,
+    featured,
+    languages,
+    tools,
+  } = job;
+  
+
+  const skillSet = [];
+  if (languages) {
+    skillSet.push(...languages);
+  }
+  if (tools) {
+    skillSet.push(...tools);
+  }
+
   return (
-    <div>
-      <p> {props.job.role} </p>
+    <div
+      className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${
+        featured && `border-l-4 border-teal-500 border-solid`
+      } sm:flex-row`}
+    >
+      <div>
+        <img className="-mt-16 mb-4 w-20 h-20 sm:mt-0 sm:h-24 sm:w-24" src={logo} alt={company}></img>
+      </div>
+
+      <div className="flex flex-col justify-between ml-4">
+        <h3 className="font-bold text-teal-500 uppercase">
+          {company}
+          {newJob && (
+            <span className="text-teal-100 bg-teal-500 font-bold m-2 py-1 px-2 rounded-full">
+              New!
+            </span>
+          )}
+          {featured && (
+            <span className="text-white bg-gray-800 font-bold py-1 px-2 rounded-full">
+              Featured
+            </span>
+          )}
+        </h3>
+        <h2 className="font-bold text-xl my-2"> {position} </h2>
+        <p className="text-gray-700">
+          {postedAt} - {contract} - {location}
+        </p>
+      </div>
+
+      <div
+        className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 
+      border-solid sm:ml-auto sm:border-0 sm:mt-0 sm:pt-0"
+      >
+        {skillSet.map((skill) => (
+          <span onClick={() => handleTagClick(skill)} className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded">
+            {skill}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
